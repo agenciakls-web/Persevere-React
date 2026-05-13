ff9488c0a12e14e9d3b8fe57b571624ea9b84f02
fabiofreitasbr <fabiofreitassilvacontato@gmail.com>
2026-05-13 11:32:44 -0300

    atualizado - template

--- app/parts/estrutura/emailTemplate.tsx
+++ app/parts/estrutura/emailTemplate.tsx
@@ -3,14 +3,14 @@ import { ContentLinks } from "../dados/contentLinks";
 
 export function EmailTemplate({ body }: { body: ContatoTypes }) {
     return (
-        <div className="text-[18px] font-sans mx-auto my-4 w-[600px]">
+        <div className="text-4 font-sans mx-auto my-4 w-150">
             <div className="text-center">
                 <img
                     src="https://persevere.com.br/img/logo.png"
                     width="150"
                     height="40"
                     alt={ContentLinks.titleSite}
-                    className="mx-auto w-[150px] h-[40px]"
+                    className="mx-auto w-32"
                 />
                 <h2 className="uppercase font-bold mt-2">Contato Recebido</h2>
             </div>
@@ -20,22 +20,22 @@ export function EmailTemplate({ body }: { body: ContatoTypes }) {
                 <table className="w-full border-collapse">
                     <tbody>
                         <tr>
-                            <td className="px-4 py-2 text-right font-bold text-[#efefef] w-[120px] bg-blue-600">
+                            <td className="px-4 py-2 text-right font-bold text-orange-500 w-32 bg-blue-600">
                                 Nome
                             </td>
-                            <td className="px-4 py-2 bg-[#dedede]">{body.nome}</td>
+                            <td className="px-4 py-2 bg-zinc-200">{body.nome}</td>
                         </tr>
                         <tr>
-                            <td className="px-4 py-2 text-right font-bold text-[#efefef] w-[120px] bg-blue-600">
+                            <td className="px-4 py-2 text-right font-bold text-orange-500 w-32 bg-blue-600">
                                 E-mail
                             </td>
-                            <td className="px-4 py-2 bg-[#dedede]">{body.email}</td>
+                            <td className="px-4 py-2 bg-zinc-200">{body.email}</td>
                         </tr>
                         <tr>
-                            <td className="px-4 py-2 text-right font-bold text-[#efefef] w-[120px] bg-blue-600">
+                            <td className="px-4 py-2 text-right font-bold text-orange-500 w-32 bg-blue-600">
                                 Telefone
                             </td>
-                            <td className="px-4 py-2 bg-[#dedede]">{body.telefone}</td>
+                            <td className="px-4 py-2 bg-zinc-200">{body.telefone}</td>
                         </tr>
                     </tbody>
                 </table>
