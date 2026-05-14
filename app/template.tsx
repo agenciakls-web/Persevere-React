06b73e9c7920369ee26bb09e3ea768f2888bbbb3
fabiofreitasbr <fabiofreitassilvacontato@gmail.com>
2026-05-14 15:12:35 -0300

    atualizado

--- app/template.tsx
+++ app/template.tsx
@@ -2,7 +2,7 @@
     "use client";
 // import Footer from "./parts/estrutura/Footer";
 // import Menu from "./parts/estrutura/menu";
-import { usePathname } from 'next/navigation';
+// import { usePathname } from 'next/navigation';
 
 
 export default function Template({ children }: { children: React.ReactNode }) {
