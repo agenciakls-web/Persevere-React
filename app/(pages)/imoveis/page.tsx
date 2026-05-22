24c2e42bf0f93c54daaf42620eeb012c12437048
fabiofreitasbr <fabiofreitassilvacontato@gmail.com>
2026-05-22 09:42:56 -0300

    suspense

--- app/(pages)/imoveis/page.tsx
+++ app/(pages)/imoveis/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ListImoveis from "@/app/parts/Imoveis/ListImoveis";
+import { Suspense } from "react";
 
 export default function Imoveis() {
     const exemplo = {
@@ -49,8 +50,8 @@ export default function Imoveis() {
         ],
     };
     return (
-        <>
+        <Suspense fallback={<div className="py-12 text-center">Carregando filtros...</div>}>
             <ListImoveis tiposImoveis={exemplo.tiposImoveis} />
-        </>
+        </Suspense>
     );
 }
