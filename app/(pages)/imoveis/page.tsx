d128aa1ee3eb8214b174f6dbd98b77ca25ce85f1
agenciakls-web <contato@agenciakls.com.br>
2026-05-28 18:58:39 -0300

    sem suspense

--- app/(pages)/imoveis/page.tsx
+++ app/(pages)/imoveis/page.tsx
@@ -1,12 +1,7 @@
-"use client";
-
 import ListImoveis from "@/app/parts/Imoveis/ListImoveis";
-import { Suspense } from "react";
 
 export default function Imoveis() {
     return (
-        <Suspense fallback={<div className="py-12 text-center">Carregando filtros...</div>}>
-            <ListImoveis />
-        </Suspense>
+        <ListImoveis />
     );
 }
