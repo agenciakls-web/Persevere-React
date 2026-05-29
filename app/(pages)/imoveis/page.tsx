3a068b105592b03b883201bc0dd9e62152354e58
agenciakls-web <contato@agenciakls.com.br>
2026-05-29 00:05:23 -0300

    dunamic - force

--- app/(pages)/imoveis/page.tsx
+++ app/(pages)/imoveis/page.tsx
@@ -3,6 +3,9 @@
 import ListImoveis from "@/app/parts/Imoveis/ListImoveis";
 import { Suspense } from "react";
 
+export const dynamic = 'force-dynamic'; 
+export const revalidate = 0;
+
 export default function Imoveis() {
     return (
         <Suspense fallback={<div className="py-12 text-center">Carregando filtros...</div>}>
