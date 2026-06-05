e74c7bc99a37f8795500ed375fa88deb0ef28401
agenciakls-web <contato@agenciakls.com.br>
2026-06-05 01:36:20 -0300

    atualizado

--- app/parts/Home/Banner.tsx
+++ app/parts/Home/Banner.tsx
@@ -11,17 +11,17 @@ export default async function Banner() {
     
     // Sua lista local mantida no servidor do Front-end
     const slidesLocais: Slide[] = [
-        { codigo: "PSI004", img: "https://persevere.com.br/img/slide/PSI004.jpg" },
-        { codigo: "ps850", img: "https://persevere.com.br/img/slide/ps850.jpg" },
-        { codigo: "PSI138", img: "https://persevere.com.br/img/slide/PSI138.jpg" },
-        { codigo: "PS1086", img: "https://persevere.com.br/img/slide/PS1086.jpg" },
-        { codigo: "PS1088", img: "https://persevere.com.br/img/slide/PS1088.jpg" },
-        { codigo: "PS1077", img: "https://persevere.com.br/img/slide/PS1077.jpg" },
-        { codigo: "ps1032", img: "https://persevere.com.br/img/slide/ps1032.jpg" },
-        { codigo: "PS899", img: "https://persevere.com.br/img/slide/PS899.jpg" },
-        { codigo: "ps1089", img: "https://persevere.com.br/img/slide/ps1089.jpg" },
-        { codigo: "PSI074", img: "https://persevere.com.br/img/slide/PSI074.jpg" },
-        { codigo: "ps688", img: "https://persevere.com.br/img/slide/ps688.jpg" },
+        { codigo: "PS899", img: "/img/slide/PS899.jpg" },
+        { codigo: "PSI004", img: "/img/slide/PSI004.jpg" },
+        { codigo: "ps850", img: "/img/slide/ps850.jpg" },
+        { codigo: "PSI138", img: "/img/slide/PSI138.jpg" },
+        { codigo: "PS1086", img: "/img/slide/PS1086.jpg" },
+        { codigo: "PS1088", img: "/img/slide/PS1088.jpg" },
+        { codigo: "PS1077", img: "/img/slide/PS1077.jpg" },
+        { codigo: "ps1032", img: "/img/slide/ps1032.jpg" },
+        { codigo: "ps1089", img: "/img/slide/ps1089.jpg" },
+        { codigo: "PSI074", img: "/img/slide/PSI074.jpg" },
+        { codigo: "ps688", img: "/img/slide/ps688.jpg" },
     ];
 
     let slides: Slide[] = [];
